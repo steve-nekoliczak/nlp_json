4bbb97ea455b67d7bb050b471c1c515f5d697110
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2019-05-03 20:59:39 -0500

    Changing features from being an object to being a dictionary. This makes the this program simpler and more flexible.

--- models.py
+++ models.py
@@ -1,55 +1,16 @@
 from marshmallow import fields, Schema
 
 
-class Features:
-    def __init__(self):
-        # shared article/adjective/pronoun/verb feats
-        self.number = ""
-        # shared article/adjective/pronoun feats
-        self.case = ""
-        self.gender = ""
-        # shared article/pronoun feats
-        self.pron_type = ""
-        # shared pronoun/verb feats
-        self.person = ""
-        # only article feats
-        self.definite = ""
-        # only verb feats
-        self.mood = ""
-        self.tense = ""
-        self.verb_form = ""
-
-
 class Word:
     def __init__(self):
         self.text = ""
         self.lemma = ""
         self.pos = ""
-        self.feats = Features()
-
-
-class FeaturesSchema(Schema):
-    # shared article/adjective/pronoun/verb feats
-    case = fields.Str()
-    # shared article/adjective/pronoun feats
-    number = fields.Str()
-    gender = fields.Str()
-    # shared article/pronoun feats
-    pron_type = fields.Str()
-    # shared pronoun/verb feats
-    person = fields.Str()
-    # only article feats
-    definite = fields.Str()
-    # only verb feats
-    mood = fields.Str()
-    tense = fields.Str()
-    verb_form = fields.Str()
+        self.feats = {}
 
 
 class WordSchema(Schema):
     text = fields.Str()
     lemma = fields.Str()
     pos = fields.Str()
-    feats = fields.Nested(FeaturesSchema())
-
-
+    feats = fields.Dict()
--- nlp_processor.py
+++ nlp_processor.py
@@ -15,25 +15,7 @@ def process_sentences(sentences_str):
             word_obj.lemma = word.lemma
             word_obj.pos = word.upos
 
-            feats = dict(x.split('=') for x in word.feats.split('|') if "=" in x)
-            if 'Case' in feats:
-                word_obj.feats.case = feats['Case']
-            if 'Gender' in feats:
-                word_obj.feats.gender = feats['Gender']
-            if 'Number' in feats:
-                word_obj.feats.number = feats['Number']
-            if 'PronType' in feats:
-                word_obj.feats.pron_type = feats['PronType']
-            if 'Person' in feats:
-                word_obj.feats.person = feats['Person']
-            if 'Definite' in feats:
-                word_obj.feats.definite = feats['Definite']
-            if 'Mood' in feats:
-                word_obj.feats.mood = feats['Mood']
-            if 'Tense' in feats:
-                word_obj.feats.tense = feats['Tense']
-            if 'VerbForm' in feats:
-                word_obj.feats.verb_form = feats['VerbForm']
+            word_obj.feats = dict(x.split('=') for x in word.feats.split('|') if "=" in x)
 
             sentences_json[i].append(dummy_schema.dump(word_obj).data)
 
