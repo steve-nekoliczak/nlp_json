f68e37204140721050972c61b0fdeb143df598af
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2019-05-30 17:13:42 -0500

    Adding rudimentary functionality to handle contractions. Also fixing base-1 indexing to be base-0.

--- nlp_processor.py
+++ nlp_processor.py
@@ -9,12 +9,17 @@ def process_sentences(sentences_str):
     dummy_schema = WordSchema()
     for i, sentence in enumerate(sentences):
         sentences_json.append([])
-        for word in sentence.words:
+        for j, token in enumerate(sentence.tokens):
             word_obj = Word()
-            word_obj.text = word.text
-            word_obj.lemma = word.lemma
+            word_obj.text = token.text
+            if len(token.words) > 1:
+                word = token.words[1]
+                word_obj.lemma = token.words[0].lemma + ' ' + token.words[1].lemma
+            else:
+                word = token.words[0]
+                word_obj.lemma = word.lemma
             word_obj.pos = word.upos
-            word_obj.index = word.index
+            word_obj.index = j
 
             word_obj.feats = dict(x.split('=') for x in word.feats.split('|') if "=" in x)
 
